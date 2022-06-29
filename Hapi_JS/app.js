a7695c43f1f1a233cf11cb0ae0771458beb8a33b
Cuong15061999 <phamvqcuong99@gmail.com>
2022-06-29 18:10:10 +0700

    CRUD User, Open Api Swagger, Joi Validation

--- Hapi_JS/app.js
+++ Hapi_JS/app.js
@@ -1,12 +1,47 @@
 //Import important lib
 const Hapi = require('@hapi/hapi');
 var connect = require('./config/mongoConnect');
+const userRoutes = require('./routes/userRoutes')
+const Pack = require('./package');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
 
-//Setup Server
-const server = new Hapi.Server({"host": "localhost","port": 8080})
 
-//Connect to Mongodb compass
-connect.connectDB()
+const init = async () => {
+    //Setup Server
+    const server = new Hapi.Server({
+        host: "localhost",
+        port: 8080
+    });
+    //Connect to Mongodb compass
+    connect.connectDB()
 
-//Separate Routes
-server.start();
+    //Setup swagger
+    const swaggerOptions = {
+        info: {
+            title: 'Test API Documentation',
+            version: Pack.version,
+        },
+    };
+
+    //Wait for server regist swagger api
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+
+    //Separate Routes
+    await userRoutes(server)
+
+    //start
+    await server.start();
+};
+init();
+
+
+

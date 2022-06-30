f02b8e4cf47cf45ad7c1cfbc8248529036af757d
Cuong15061999 <phamvqcuong99@gmail.com>
2022-06-30 19:42:39 +0700

    Set up agenda to run report connection every 1m

--- Hapi_JS/app.js
+++ Hapi_JS/app.js
@@ -6,7 +6,13 @@ const Pack = require('./package');
 const Inert = require('@hapi/inert');
 const Vision = require('@hapi/vision');
 const HapiSwagger = require('hapi-swagger');
-
+const Agenda = require("agenda");
+const agenda = new Agenda({ db: { address: "mongodb+srv://phamvqcuong99:Quoccuong_999@cluster0.7qnaw.mongodb.net/Learning_Hapi" } });
+agenda.define("report", async (job) => {
+    console.log("connection still fine")
+});
+const Queue = require('bull');
+const videoQueue = new Queue('video transcoding', 'redis://127.0.0.1:6379');
 
 const init = async () => {
     //Setup Server
@@ -34,9 +40,29 @@ const init = async () => {
             options: swaggerOptions
         }
     ]);
+    //agenda connection report every 1 minute
+    await agenda.start();
+    await agenda.every("1 minute", "report");
+
+    //bull test
+    await videoQueue.process(function (job, done) {
+        // transcode video asynchronously and report progress
+        job.progress(42);
+
+        // call done when finished
+        done();
+
+        // or give an error if error
+        done(new Error('error transcoding'));
+
+        // or pass it a result
+        done(null, { framerate: 29.5 /* etc... */ });
+        throw new Error('some unexpected error');
+    });
+    await videoQueue.add({ video: 'http://example.com/video1.mov' });
 
     //Separate Routes
-    await userRoutes(server)
+    userRoutes(server)
 
     //start
     await server.start();

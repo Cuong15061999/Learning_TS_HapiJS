9ff1e81c03bd4ede49da349918c781cf536cae57
Cuong15061999 <phamvqcuong99@gmail.com>
2022-07-04 17:40:29 +0700

    Agenda, swagger, have bug in Joi

--- Hapi_TS/src/app.ts
+++ Hapi_TS/src/app.ts
@@ -2,17 +2,57 @@ import Hapi from "@hapi/hapi";
 import { Server } from "@hapi/hapi";
 import { mongoconnect } from "./config/mongoconnect";
 import { userRoutes } from "./routes/userRoutes";
+import inert from "@hapi/inert";
+import vision from "@hapi/vision"
+import hapiswagger from "hapi-swagger";
+import { Agenda } from "agenda";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+//set up agenda queue
+var URL = <string> process.env.MongoURL;
+const agenda = new Agenda({ db: { address: URL } });
+agenda.define("report", async () => {
+    console.log("connection still fine")
+});
 
 const init = async () => {
+    //set up server
     const server: Server = Hapi.server({
         port: 3000,
         host: 'localhost',
     });
+    //set up open API Swagger
+    const swaggerOptions: hapiswagger.RegisterOptions = {
+        info:{
+            title:'Test API Documentation'
+        }
+    };
+    const plugins: Array<Hapi.ServerRegisterPluginObject<any>> = [
+        {
+            plugin: inert
+        },
+        {
+            plugin: vision
+        },
+        {
+            plugin: hapiswagger,
+            options: swaggerOptions
+        }
+    ]
+    await server.register(plugins);
+
+    //connect DB
     mongoconnect.connectDB()
+
+    //Agenda
+    await agenda.start();
+    await agenda.every("1 minute", "report");
 
     //Separate routes
     userRoutes(server)
 
+    //start server
     await server.start();
     console.log('Server running on %s', server.info.uri);
     
--- Hapi_TS/src/routes/userRoutes.ts
+++ Hapi_TS/src/routes/userRoutes.ts
@@ -1,36 +1,66 @@
 import { Server } from "@hapi/hapi";
+import Joi from "@hapi/joi";
 import { createUser, getAllUser, getUser, updateUser, deleteUser } from "../services/userServices";
 
-
+const userPayload = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+    fullName: Joi.string().required()
+})
 export const userRoutes = (server: Server) => {
     //create new user
     server.route({
         method: "POST",
-        path: "/users",
+        path: "/users",        
+        options: {
+            description: 'Create new user',
+            notes: 'Create new user',
+            tags: ['api'],
+        },
         handler: createUser
     });
     //get all user
     server.route({
         method: "GET",
         path: "/users",
+        options: {
+            description: 'Create new user',
+            notes: 'Create new user',
+            tags: ['api'],
+        },
         handler: getAllUser
     });
     //get user by id
     server.route({
         method: "GET",
         path: "/users/{id}",
+        options: {
+            description: 'Create new user',
+            notes: 'Create new user',
+            tags: ['api'],
+        },
         handler: getUser
     });
     //update user by id
     server.route({
         method: "PUT",
         path: "/users/{id}",
+        options: {
+            description: 'Create new user',
+            notes: 'Create new user',
+            tags: ['api'],
+        },
         handler: updateUser
     });
     //delete user by id
     server.route({
         method: "DELETE",
         path: "/users/{id}",
+        options: {
+            description: 'Create new user',
+            notes: 'Create new user',
+            tags: ['api'],
+        },
         handler: deleteUser
     });
 }
